51df2e263bce69411a05cfa01cdaa0ed6762067f
Jaidev-web <jaidevnaik26@gmail.com>
2026-09-13 00:06:25 +0530

    Update build.gradle.kts

--- flutter-app/my_app/android/app/build.gradle.kts
+++ flutter-app/my_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.safenet"
+    namespace = "com.yourcompany.my_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.safenet"
+        applicationId = "SafeNET.com"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
